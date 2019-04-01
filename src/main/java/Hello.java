63d6a54e83b1899de4fc198f904bf46c351bc462
Jill MONTELLANO <jillmontellano@ph.ibm.com>
2019-04-01 10:34:43 +0800

    edit hello world message

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -10,7 +10,7 @@ public class Hello extends HttpServlet {
 
    public void init() throws ServletException {
       // Do required initialization
-      message = "Hello World: src/main/java/Hello.java";
+      message = "Hello World I am jilliane: src/main/java/Hello.java";
    }
 
    public void doGet(HttpServletRequest request, HttpServletResponse response)
